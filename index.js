2c16c83a34eb8f4283cd314f0a0dba87cd880679
Addidaan <adriana48_rs@hotmail.com>
2023-01-17 12:43:13 -0600

    primeros cambios, funcion mdlinks, capeta cli y de ejemplo

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks = (path, options) => new Promise((resolve, reject) => {
+  // La ruta existe?
+  if (fs.existsSync(path)) {
+    resolve('si existe la ruta');
+    // Chequear o convertir a una ruta absoluta.
+    // Probar si es archivo o directorio.
+    // Si es un directorio filtrar los archivos md.
+  } else {
+    // Si no existe la ruta se rechaza
+    reject('La ruta no existe');
+  }
+});
+console.log(fs.existsSync('C:/Users/adria/Desktop/Laboratoria/DEV001-md-links/prueba/ejemplo.md'));
+
+module.exports = {
+  mdLinks,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,13 @@
-const mdLinks = require('../');
-
+const { mdLinks } = require('../index');
 
 describe('mdLinks', () => {
-
   it('should...', () => {
     console.log('FIX ME!');
   });
-
+  // it('Deberia devolver una promesa', () => {
+  //   expect(mdLinks()).reolves.toBe(typeof Promise);
+  // });
+  it('Debe rechazar cuando el path no exixte', () => (mdLinks('/path/noexiste.md')).catch((error) => {
+    expect(error).toBe('La ruta no existe');
+  }));
 });
