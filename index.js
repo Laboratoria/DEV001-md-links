691a82e8334ac85dc5c265dda8a4c5abd2d17432
GriseldaAlonso <gri_alonso@hotmail.com>
2023-01-18 17:59:57 -0500

    función recursiva para leer directorios

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const {
   pathExist,
   getAbsolutePath,
   isFileMd,
-  readFile
+  readFile,
+  searchFilesMd
 } = require('./path.js')
 
 const mdLinks = (path, options) => new Promise((resolve, reject) => {
   // identificar si la ruta existe
   if (!pathExist(path)) {
     // si no existe la ruta, rechaza la promesa
-    console.log('la ruta no existe');
     return reject('La ruta no existe');
-    
   } else {
     // chequear o convertir a ruta absoluta
     const pathAbsolute = getAbsolutePath(path)
+    console.log(pathAbsolute);
     // chequear si la ruta es un archivo md
     if (!isFileMd(pathAbsolute)) {
-      return reject('No es un archivo md')
+      searchFilesMd(pathAbsolute)
     } else {
       //leer los archivos md
-      readFile(pathAbsolute);
+      readFile(pathAbsolute)
+      console.log(searchFilesMd, 'llegamos hasta aquí')
     }
     // identificar si es un archivo o un directorio
     // si es un directorio, leer el directorio
--- path.js
+++ path.js
@@ -9,6 +9,7 @@ const getAbsolutePath = (paths) => {
   return path.isAbsolute(paths) ? paths : path.resolve(paths);
 };
 
+// chequear si la extensión corresponde a un archivo md
 const isFileMd = (pathAbsolute) => {
   const filePath = path.extname(pathAbsolute);
   if (filePath === '.md') {
@@ -16,16 +17,47 @@ const isFileMd = (pathAbsolute) => {
   }
   return false;
 };
-
+// leer el archivo si es una extensión md
 const readFile = (pathsMd) => {
   if (pathsMd === true) {
     return fs.readFileSync(pathsMd, 'utf-8');
   }
 };
 
+// evaluar si el path corresponde a un directorio
+const isDirectory = (pathAbsolute) => fs.lstatSync(pathAbsolute).isDirectory();
+
+
+// función para leer el directorio buscando archivos md
+const arrayFileMd = [];
+const searchFilesMd = (param) => {
+  if (!isDirectory(param) && isFileMd(param)){
+    arrayFileMd.push(param)
+  } else {
+    // constante para leer el directorio
+    const readDir = fs.readdirSync(param);
+    readDir.forEach((list) => {
+      //separando cada ruta dentro del directorio
+      list = path.join(param, list);
+       //console.log('cada ruta dentro del directorio: ', list);
+       // recursividad para leer un directorio
+      if (isDirectory(list)) {
+        searchFilesMd(list);
+      } else if (isFileMd(list)) {
+        // guardando cada archivo md dentro del array
+        arrayFileMd.push(list);
+      }
+    });
+  } 
+  console.log('estamos en arrayFileMd', arrayFileMd);
+  return arrayFileMd;
+};
+
+
 module.exports = {
   pathExist,
   getAbsolutePath,
   isFileMd,
-  readFile
+  readFile,
+  searchFilesMd
 };
