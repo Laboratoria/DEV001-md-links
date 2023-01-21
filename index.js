2bd70ecbc030c3ab266ad5cfac9077daac33fdc5
Januha88 <januharyg@gmail.com>
2023-01-20 22:39:18 -0300

    confg jason

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = () => {
-  // ...
+// NOS PERMITE TRABAJAR CON ARCHIVOS DEL SISTEMA OPERATIVO.
+const fs = require('fs');
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    //identifica si la ruta existe.
+    if (fs.existsSync(path)) {
+      //chequear y convertir  a una ruta absoluta.
+      //probar si esa ruta absoluta es un archivo o un directorio.
+      // si es un directorio extraer los archivos md y devolverlo en un arreglo.
+    } else {
+      // si no existe la ruta se rechaza la promesa.
+      reject('la ruta no existe');
+    }
+  });
 };
+
+module.exports = {
+  mdLinks,
+}
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,18 @@
-const mdLinks = require('../');
+const { mdLinks } = require('../index.js');
 
 
 describe('mdLinks', () => {
-
   it('should...', () => {
     console.log('FIX ME!');
   });
 
+  //it('debería devolver una promesa', () => {
+  // expect(mdLinks()).toBe(typeof Promise);
+  // });
+
+  it('Debe rechazar cuando el path no existe', () => {
+    mdLinks('/januhary/cursos/noexiste.md').catch((error) => {
+      expect(error).toBe('la ruta no existe');
+    })
+  });
 });
