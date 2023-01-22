a3ac48007b3f65330b973751b4455b2557b12ec4
Addidaan <adriana48_rs@hotmail.com>
2023-01-22 13:43:01 -0600

    agregue funcion path is absolute y turnPathAbsolute, no pasa teste pathIsAbsulute

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,13 +1,33 @@
 const { mdLinks } = require('../index');
 
+// describe('mdLinks', () => {
+//   it('should...', () => {
+//     console.log('FIX ME!');
+//   });
+// it('Deberia devolver una promesa', () => {
+//   expect(mdLinks()).reolves.toBe(typeof Promise);
+// });
 describe('mdLinks', () => {
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-  // it('Deberia devolver una promesa', () => {
-  //   expect(mdLinks()).reolves.toBe(typeof Promise);
-  // });
   it('Debe rechazar cuando el path no exixte', () => (mdLinks('/path/noexiste.md')).catch((error) => {
-    expect(error).toBe('La ruta no existe');
+    expect(error).toStrictEqual(new Error('La ruta no existe'));
   }));
 });
+
+describe('mdLinks', () => {
+  it('Debe indicar si la ruta es absoluta o no', () => mdLinks('C:/Users/adria/Desktop/Laboratoria/DEV001-md-links/prueba/ejemplo.md').then((value) => {
+    expect(value).toEqual(true);
+  }));
+});
+
+describe('mdLinks', () => {
+  it('Si la ruta es relativa cambia a absoluta', () => mdLinks('./prueba/ejemplo.md').then((value) => {
+    expect(value).toEqual('C:\\Users\\adria\\Desktop\\Laboratoria\\DEV001-md-links\\prueba\\ejemplo.md');
+  }));
+});
+
+
+// describe('mdLinks', () => {
+//   it('Debe ver si la ruta es absoluta o no', () => pathIsAbsolute('C:/Users/adria/Desktop/Laboratoria/DEV001-md-links/prueba/ejemplo.md').then((value) => {
+//     expect(value).toBe(turnPathAbsolute());
+//   }));
+// });
